fab0e69f7aed24305e35c9d98dde322a21da0036
gsec <gui@posteo.net>
2022-09-26 23:52:05 +0200

    feat: wrapped into funcs, called from main.

--- alias2csv.py
+++ alias2csv.py
@@ -1,24 +1,40 @@
-INPUT_FNAME = "alias"
-OUTPUT_FNAME = "alias.csv"
-
-with open(INPUT_FNAME, "r") as handler:
-    data = handler.read()
-
-
 def process(line):
-    try:
-        a, nick, first, last, mail = line.split(" ")
-    except Exception as ex:
-        print("Error")
-        return
+    """Reformat a line from mutt-alias to csv.
+
+    Assumes following format:
+    'alias NICK_NAME FIRST_NAME LAST_NAME <EMAIL>'
+    """
+    alias_literal, nick, first, last, mail = line.split(" ")
     smail = mail.strip("><")
     return ",".join([first, last, smail, nick])
 
 
-lines = data.split("\n")
-header = "first name,last name,email-address,nickname"
-processed = "\n".join(process(line) for line in lines if line)
-output = header + "\n" + processed
+def gen_output(data):
+    header = ["first name,last name,email-address,nickname"]
+    lines = data.split("\n")
+    for idx, line in enumerate(lines):
+        if not line:
+            continue
+        try:
+            header.append(process(line))
+        except Exception as ex:
+            print(f"ERROR: '{ex}'\nSkippd line {idx}:\t'{line}'")
 
-with open(OUTPUT_FNAME, "w") as handler:
-    handler.write(output)
+    return "\n".join(header)
+
+
+def main():
+    INPUT_FNAME = "alias"
+    OUTPUT_FNAME = "alias.csv"
+
+    with open(INPUT_FNAME, "r") as handler:
+        data = handler.read()
+
+    with open(OUTPUT_FNAME, "w") as handler:
+        handler.write(gen_output(data))
+
+    print("All good, output to:", OUTPUT_FNAME)
+
+
+if __name__ == "__main__":
+    main()
